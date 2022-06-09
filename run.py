8ae1dcedef531b0b88483e871b75c31815cb8252
EJDiamond <emilydiamond321@gmail.com>
2022-06-09 14:04:10 +0000

    Create secret word function to pull a random word from the google sheet

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ Import the spreadsheet to retrieve the secret words
 """
 import gspread
 from google.oauth2.service_account import Credentials
+from random import randrange
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,12 +15,22 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread. authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_words')
+WORDS = SHEET.worksheet('words')
 
-words = SHEET.worksheet('words')
 
-data = words.get_all_values()
+def secret_word():
+    """
+    Pulling a random word from the word column in the hangman_words google sheet to use as secret word
+    """
+    row_count = len(WORDS.col_values(1))
+    row_ref_start = row_count +1
+    random_row = WORDS.row_values(randrange(1, row_ref_start))
+    random_word = random_row[0]
+    
 
-print(data)
+    print(random_word)
+
+secret_word()
 
 
 def view_hangman(tries):
@@ -32,7 +43,7 @@ def view_hangman(tries):
                 |   -------+     |
                 |   |      |     |
                 |   |      0     |
-                |   |     /|\\    |
+                |   |     /|\\   |
                 |   |     / \    |
                 |   |            |
                 |   ----------   |
